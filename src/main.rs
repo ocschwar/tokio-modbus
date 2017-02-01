af855aaf193132055bd9556ed85cc1cfd6494a28
Omri Schwarz <oschwarz@cse.fraunhofer.org>
2017-01-31 20:53:03 -0500

    Add a test suite.

--- src/main.rs
+++ src/main.rs
@@ -634,34 +634,4 @@ fn main() {
         .serve(move || Ok(ModbusService::new(block.clone())));
 }
 
-#[cfg(test)]
-mod tests {
-    use super::{BlankRegisters,ModbusRequestPDU,ModbusResponsePDU,FunctionCode};
-    #[test]
-    fn test_read_coils(){
-        let mut br = BlankRegisters::new();
-        let req = ModbusRequestPDU {
-            code: FunctionCode::ReadCoils as u8,
-            address: 1 as u16,
-            q_or_v:10 as u16,
-            addl:None
-        };
-        let resp = br.call(req.clone());
-        match resp {
-            ModbusResponsePDU::ReadCoilsResponse {
-                code : code,
-                byte_count: byte_count,
-                coil_status:coil_status   }=> {
-                assert!( code == req.code);
-                assert!( coil_status.len() == (req.q_or_v /8 +1)  as usize);
-                for i in 0..byte_count {
-                    assert!( 0 as u8 == coil_status[i as usize]);
-                }
-            },
-            _  => {
-                assert!(false);
-            }
-        };
-        
-    }
-}
+mod test;
---
+++ src/test.rs
@@ -0,0 +1,37 @@
+//
+use BlankRegisters;
+use ModbusRequestPDU;
+use ModbusResponsePDU;
+use FunctionCode;
+
+#[cfg(test)]
+mod tests {
+    use super::{BlankRegisters,ModbusRequestPDU,ModbusResponsePDU,FunctionCode};
+    #[test]
+    fn test_read_coils(){
+        let mut br = BlankRegisters::new();
+        let req = ModbusRequestPDU {
+            code: FunctionCode::ReadCoils as u8,
+            address: 1 as u16,
+            q_or_v:10 as u16,
+            addl:None
+        };
+        let resp = br.call(req.clone());
+        match resp {
+            ModbusResponsePDU::ReadCoilsResponse {
+                code : code,
+                byte_count: byte_count,
+                coil_status:coil_status   }=> {
+                assert!( code == req.code);
+                assert!( coil_status.len() == (req.q_or_v /8 +1)  as usize);
+                for i in 0..byte_count {
+                    assert!( 0 as u8 == coil_status[i as usize]);
+                }
+            },
+            _  => {
+                assert!(false);
+            }
+        };
+        
+    }
+}
